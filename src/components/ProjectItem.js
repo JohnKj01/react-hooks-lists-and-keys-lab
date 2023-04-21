23f6513cc7db6049d93e4df21f51e8958ea6c491
John <johnkaranjairungu@gmail.com>
2023-04-21 15:52:00 +0300

    added react components to the website

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const items = [];
+  for (var i =0; i<technologies.length; i++){
+    items.push(
+      <span key={i}>{technologies[i]}</span>
+    );
+  }
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {items}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,27 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  const items = [];
+
+  projects.forEach((element, index) => {
+    items.push(
+      <ProjectItem
+        key={index}
+        name={element.name}
+        about={element.about}
+        technologies={element.technologies}
+      />
+    );
+  });
+
+
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {items}
+      </div>
     </div>
   );
 }
